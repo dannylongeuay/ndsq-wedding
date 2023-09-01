1d3d5d11a63944e27a8b5993e16d3a775296ada5
dannylongeuay <dannylongeuay@yahoo.com>
2023-08-31 23:11:23 -0700

    revert(*): :rewind: revert to mobile clips width commit

--- src/main.rs
+++ src/main.rs
@@ -8,47 +8,58 @@ fn main() {
 fn App(cx: Scope) -> impl IntoView {
     let (selected, set_selected) = create_signal(cx, SlotSelection::Landing);
     view! { cx,
-        <div id="scroll" class="scroll">
+        <Header selection=selected set_selection=set_selected/>
         <main class="container">
         <Slot selection=selected>
             <Landing slot >
                 <LandingSlot set_selection=set_selected/>
             </Landing>
             <Photos slot>
-                <PhotosSlot set_selection=set_selected/>
+                <PhotosSlot/>
             </Photos>
             <Photobooth slot>
-                <PhotoboothSlot set_selection=set_selected/>
+                <PhotoboothSlot/>
             </Photobooth>
             <Clips slot>
-                <ClipsSlot set_selection=set_selected/>
+                <ClipsSlot/>
             </Clips>
         </Slot>
         </main>
-        </div>
     }
 }
 
 #[component]
 fn Header(
     cx: Scope,
+    selection: ReadSignal<SlotSelection>,
     set_selection: WriteSignal<SlotSelection>,
-    interval_handle: IntervalHandle,
 ) -> impl IntoView {
     view! { cx,
-        <nav class="container-fluid">
-            <ul>
-                <li>
-                    <button
-                    id="back"
-                    on:click=move |_| {
-                        set_selection.set(SlotSelection::Landing);
-                        interval_handle.clear();
-                    }
-                    >Back</button>
-                </li>
-            </ul>
-        </nav>
+        { move || if selection.get() != SlotSelection::Landing {
+                view! { cx,
+                    <nav class="container-fluid">
+                        <ul>
+                            <li>
+                                <button
+                                on:click=move |_| { set_selection.set(SlotSelection::Landing)}
+                                >Back</button>
+                            </li>
+                        </ul>
+                    </nav>
+                }
+            } else {
+                view! { cx,
+                    <nav class="container-fluid">
+                        <ul/>
+                        <ul>
+                            <li>
+                                <h1>"ND Wedding Gallery"</h1>
+                            </li>
+                        </ul>
+                        <ul/>
+                    </nav>
+                }
+        }}
     }
 }
 
@@ -85,15 +96,6 @@ struct Landing {
 #[component]
 fn LandingSlot(cx: Scope, set_selection: WriteSignal<SlotSelection>) -> impl IntoView {
     view! { cx,
-        <nav class="container-fluid">
-            <ul/>
-            <ul>
-                <li>
-                    <h1>"ND Wedding Gallery"</h1>
-                </li>
-            </ul>
-            <ul/>
-        </nav>
         <article>
             <header>Photos</header>
             <section>
@@ -104,10 +106,7 @@ fn LandingSlot(cx: Scope, set_selection: WriteSignal<SlotSelection>) -> impl Int
             </section>
             <button
             on:click=move |_| {
-                document()
-                    .get_element_by_id("scroll")
-                    .expect("scroll div to be present")
-                    .scroll_to_with_x_and_y(0.0, 0.0);
+                document().body().expect("body to be present").scroll_into_view();
                 set_selection.set(SlotSelection::Photos);
             }
             >View</button>
@@ -122,10 +121,7 @@ fn LandingSlot(cx: Scope, set_selection: WriteSignal<SlotSelection>) -> impl Int
             </section>
             <button
             on:click=move |_| {
-                document()
-                    .get_element_by_id("scroll")
-                    .expect("scroll div to be present")
-                    .scroll_to_with_x_and_y(0.0, 0.0);
+                document().body().expect("body to be present").scroll_into_view();
                 set_selection.set(SlotSelection::Photobooth);
             }
             >View</button>
@@ -140,10 +136,7 @@ fn LandingSlot(cx: Scope, set_selection: WriteSignal<SlotSelection>) -> impl Int
             <button
             on:click=move |_| { set_selection.set(SlotSelection::Clips)}
             on:click=move |_| {
-                document()
-                    .get_element_by_id("scroll")
-                    .expect("scroll div to be present")
-                    .scroll_to_with_x_and_y(0.0, 0.0);
+                document().body().expect("body to be present").scroll_into_view();
                 set_selection.set(SlotSelection::Clips);
             }
             >View</button>
@@ -154,7 +147,6 @@ fn LandingSlot(cx: Scope, set_selection: WriteSignal<SlotSelection>) -> impl Int
 #[component]
 fn InfiniteScroller(
     cx: Scope,
-    set_selection: WriteSignal<SlotSelection>,
     #[prop(default = "https://ik.imagekit.io/dannylongeuay/ndsq/nd_fave_{}.jpg?tr=h-800")]
     url_format: &'static str,
     #[prop(default = "nd_fave_{}")] alt_format: &'static str,
@@ -165,24 +157,33 @@ fn InfiniteScroller(
     let initial_indexes: Vec<i32> = (photos_start..=photos_end).collect();
     let (photo_ids, set_photo_ids) = create_signal(cx, initial_indexes);
     let add_photos = move || {
-        if let Some(scroll_div) = document().get_element_by_id("scroll") {
-            let scroll_top = scroll_div.scroll_top();
-            let client_height = scroll_div.client_height();
-            let scroll_height = scroll_div.scroll_height();
-            if scroll_top + client_height >= scroll_height - 400 {
-                set_photo_ids.update(|photo_ids| {
-                    photo_ids.remove(0);
-                    if let Some(last) = photo_ids.last() {
-                        photo_ids.push((last % photos_length) + 1);
-                    }
-                });
-            }
+        let scroll_y = window().scroll_y().unwrap();
+        let inner_height = window().inner_height().unwrap().as_f64().unwrap();
+        let doc_height = document()
+            .body()
+            .expect("body to be present")
+            .offset_height();
+        if (doc_height as f64) < inner_height * 2.0 {
+            return;
+        }
+        if scroll_y + inner_height >= (doc_height - (doc_height / 5)) as f64 {
+            set_photo_ids.update(|photo_ids| {
+                photo_ids.remove(0);
+                if let Some(last) = photo_ids.last() {
+                    photo_ids.push((last % photos_length) + 1);
+                }
+            });
         }
     };
-    let handle_res = set_interval_with_handle(add_photos, std::time::Duration::from_millis(1000));
-    let interval_handle = handle_res.unwrap();
+    let scrolled = move |_| {
+        add_photos();
+    };
+    let scrollend = move |_| {
+        add_photos();
+    };
+    window_event_listener(ev::scroll, scrolled);
+    window_event_listener(ev::scrollend, scrollend);
     view! { cx,
-        <Header set_selection interval_handle/>
         <For
             each=move || photo_ids.get()
             key=|&photo_id| photo_id
@@ -205,9 +206,9 @@ struct Photos {
 }
 
 #[component]
-fn PhotosSlot(cx: Scope, set_selection: WriteSignal<SlotSelection>) -> impl IntoView {
+fn PhotosSlot(cx: Scope) -> impl IntoView {
     view! { cx,
-        <InfiniteScroller set_selection=set_selection/>
+        <InfiniteScroller/>
     }
 }
 
@@ -217,10 +218,9 @@ struct Photobooth {
 }
 
 #[component]
-fn PhotoboothSlot(cx: Scope, set_selection: WriteSignal<SlotSelection>) -> impl IntoView {
+fn PhotoboothSlot(cx: Scope) -> impl IntoView {
     view! { cx,
         <InfiniteScroller
-            set_selection
             url_format="https://ik.imagekit.io/dannylongeuay/ndsq/nd_photobooth_{}.jpg?tr=h-800"
             alt_format="nd_photobooth_{}"
             photos_length=58
@@ -234,7 +234,7 @@ struct Clips {
 }
 
 #[component]
-fn ClipsSlot(cx: Scope, set_selection: WriteSignal<SlotSelection>) -> impl IntoView {
+fn ClipsSlot(cx: Scope) -> impl IntoView {
     let clips = vec![
         "nd_down_the_aisle",
         "nd_ceremony_armpull",
@@ -252,35 +252,40 @@ fn ClipsSlot(cx: Scope, set_selection: WriteSignal<SlotSelection>) -> impl IntoV
         "nd_final_send_off",
         "nd_final_kiss",
     ];
-    let clips_start = 0;
-    let clips_end = 4;
+    let clips_start = 0 as usize;
+    let clips_end = 4 as usize;
     let initial_indexes: Vec<usize> = (clips_start..=clips_end).collect();
     let (clip_ids, set_clip_ids) = create_signal(cx, initial_indexes);
     let (clip_names, set_clip_names) = create_signal(cx, clips[clips_start..=clips_end].to_vec());
     let add_clips = move || {
-        if let Some(scroll_div) = document().get_element_by_id("scroll") {
-            let scroll_top = scroll_div.scroll_top();
-            let client_height = scroll_div.client_height();
-            let scroll_height = scroll_div.scroll_height();
-            if scroll_top + client_height >= scroll_height - 200 {
-                set_clip_ids.update(|clip_ids| {
-                    clip_ids.remove(0);
-                    if let Some(last) = clip_ids.last() {
-                        clip_ids.push((last + 1) % clips.len());
-                    }
-                });
-                let mut next_clips: Vec<&str> = Vec::new();
-                for clip_id in clip_ids.get() {
-                    next_clips.push(clips[clip_id])
+        let scroll_y = window().scroll_y().unwrap();
+        let inner_height = window().inner_height().unwrap().as_f64().unwrap();
+        let doc_height = document()
+            .body()
+            .expect("body to be present")
+            .offset_height();
+        if (doc_height as f64) < inner_height * 1.5 {
+            return;
+        }
+        if scroll_y + inner_height >= (doc_height - (doc_height / 5)) as f64 {
+            set_clip_ids.update(|clip_ids| {
+                clip_ids.remove(0);
+                if let Some(last) = clip_ids.last() {
+                    clip_ids.push((last + 1) % clips.len());
                 }
-                set_clip_names.set(next_clips);
+            });
+            let mut next_clips: Vec<&str> = Vec::new();
+            for clip_id in clip_ids.get() {
+                next_clips.push(clips[clip_id])
             }
+            set_clip_names.set(next_clips);
         }
     };
-    let handle_res = set_interval_with_handle(add_clips, std::time::Duration::from_millis(1000));
-    let interval_handle = handle_res.unwrap();
+    let scrolled = move |_| {
+        add_clips();
+    };
+    window_event_listener(ev::scroll, scrolled);
     view! { cx,
-        <Header set_selection interval_handle/>
         <For
             each=move || clip_names.get()
             key=|&clip_name| clip_name
