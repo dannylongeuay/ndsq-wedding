c5fa096a12e1db68dde66d68f273ac9803b2b988
dannylongeuay <dannylongeuay@yahoo.com>
2024-06-02 20:39:19 -0700

    feat(*): :sparkles: add flake.nix development environment

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    , fenix
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
+
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            alejandra
+            rust-analyzer
+            (pkgs.fenix.stable.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+          ];
+        };
+      }
+    );
+}
